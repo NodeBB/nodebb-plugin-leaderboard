99702f43a860082dd811db0fef593fabd5502050
Antosik <grig_anton96@mail.ru>
2021-09-07 10:55:01 +0300

    fix: autofix eslint errors

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const cron = require.main.require('cron').CronJob;
@@ -15,17 +14,17 @@ const plugin = module.exports;
 
 const cronJobs = [];
 
-cronJobs.push(new cron('0 0 17 * * *', function() {db.delete('users:reputation:daily');}, null, false));
-cronJobs.push(new cron('0 0 17 * * 0', function() {db.delete('users:reputation:weekly');}, null, false));
-cronJobs.push(new cron('0 0 17 1 * *', function() {db.delete('users:reputation:monthly');}, null, false));
+cronJobs.push(new cron('0 0 17 * * *', (() => { db.delete('users:reputation:daily'); }), null, false));
+cronJobs.push(new cron('0 0 17 * * 0', (() => { db.delete('users:reputation:weekly'); }), null, false));
+cronJobs.push(new cron('0 0 17 1 * *', (() => { db.delete('users:reputation:monthly'); }), null, false));
 
-plugin.init = function(params, callback) {
+plugin.init = function (params, callback) {
 	helpers.setupPageRoute(params.router, '/leaderboard/:term?', params.middleware, [], plugin.renderLeaderboard);
 	reStartCronJobs();
 	callback();
 };
 
-plugin.renderLeaderboard = async function(req, res) {
+plugin.renderLeaderboard = async function (req, res) {
 	const canView = await privileges.global.can('view:users', req.uid);
 	if (!canView) {
 		controllersHelpers.notAllowed(req, res);
@@ -36,32 +35,34 @@ plugin.renderLeaderboard = async function(req, res) {
 	if (term === 'alltime') {
 		term = '';
 	}
-	const set = 'users:reputation' + (term ? ':' + term : '');
+	const set = `users:reputation${term ? `:${term}` : ''}`;
 
 	const userData = await usersController.getUsers(set, req.uid, req.query);
 	const uids = userData.users.map(user => user && user.uid);
 	const scores = await db.sortedSetScores(set, uids);
-	userData.users.forEach(function(user, index) {
+	userData.users.forEach((user, index) => {
 		if (user) {
 			user.reputation = scores[index] || 0;
 		}
 	});
+
 	const breadcrumbs = [{
-		text: term ? (term.charAt(0).toUpperCase() + term.slice(1)) : 'Leaderboard'
+		text: term ? (term.charAt(0).toUpperCase() + term.slice(1)) : 'Leaderboard',
 	}];
 
 	if (term) {
-		breadcrumbs.unshift({text: 'Leaderboard', url: '/leaderboard'});
+		breadcrumbs.unshift({ text: 'Leaderboard', url: '/leaderboard' });
 		userData[term] = true;
 	}
 
 	userData.breadcrumbs = controllersHelpers.buildBreadcrumbs(breadcrumbs);
 	userData['section_sort-reputation'] = true;
 	userData.title = 'Leaderboard';
+
 	res.render('leaderboard', userData);
 };
 
-plugin.getNavigation = async function(core) {
+plugin.getNavigation = async function (core) {
 	core.push({
 		route: '/leaderboard',
 		title: 'Leaderboard',
@@ -69,25 +70,24 @@ plugin.getNavigation = async function(core) {
 		iconClass: 'fa-star',
 		textClass: 'visible-xs-inline',
 		text: 'Leaderboard',
-		properties: {  },
-		core: false
+		properties: {},
+		core: false,
 	});
 	return core;
 };
 
-plugin.onUpvote = function(data) {
-	var change = 0;
+plugin.onUpvote = function (data) {
+	let change = 0;
 	if (data.current === 'unvote') {
 		change = 1;
 	} else if (data.current === 'downvote') {
 		change = 2;
 	}
-
 	updateLeaderboards(change, data.owner);
 };
 
-plugin.onDownvote = function(data) {
-	var change = 0;
+plugin.onDownvote = function (data) {
+	let change = 0;
 	if (data.current === 'unvote') {
 		change = -1;
 	} else if (data.current === 'upvote') {
@@ -96,8 +96,8 @@ plugin.onDownvote = function(data) {
 	updateLeaderboards(change, data.owner);
 };
 
-plugin.onUnvote = function(data) {
-	var change = 0;
+plugin.onUnvote = function (data) {
+	let change = 0;
 	if (data.current === 'upvote') {
 		change = -1;
 	} else if (data.current === 'downvote') {
@@ -114,20 +114,20 @@ function updateLeaderboards(change, owner) {
 	}
 }
 
-plugin.deactivate = function(data) {
+plugin.deactivate = function (data) {
 	if (data.id === 'nodebb-plugin-leaderboard') {
 		pubsub.publish('nodebb-plugin-leaderboard:deactivate');
 	}
 };
 
-pubsub.on('nodebb-plugin-leaderboard:deactivate', function() {
+pubsub.on('nodebb-plugin-leaderboard:deactivate', () => {
 	stopCronJobs();
 });
 
 function reStartCronJobs() {
 	if (nconf.get('isPrimary')) {
 		stopCronJobs();
-		cronJobs.forEach(function(job) {
+		cronJobs.forEach((job) => {
 			job.start();
 		});
 	}
@@ -135,7 +135,7 @@ function reStartCronJobs() {
 
 function stopCronJobs() {
 	if (nconf.get('isPrimary')) {
-		cronJobs.forEach(function(job) {
+		cronJobs.forEach((job) => {
 			job.stop();
 		});
 	}
